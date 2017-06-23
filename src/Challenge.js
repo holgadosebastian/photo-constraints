b1134aa91acb73ef88a746e30f0e25854d60baac
SebastianZH <holgado.sebastian@gmail.com>
2017-06-22 21:21:36 -0300

    Adds Challenge component

---
+++ src/Challenge.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Challenge extends Component {
+  render() {
+    return (
+			<div className="Challenge">
+				<p className="Challenge__paragraph">
+					<span className="Challenge__text">A </span>
+					<span className="Challenge__highlight">wide angle </span>
+					<span className="Challenge__text">photo of </span>
+					<span className="Challenge__highlight">children playing</span>
+				</p>
+			</div>
+    );
+  }
+}
+
+export default Challenge;
