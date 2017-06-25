b45335f86eb473e175be97bb4700e1daec02bf08
SebastianZH <holgado.sebastian@gmail.com>
2017-06-25 20:01:48 -0300

    Adds basic challenge logic

--- src/Challenge.js
+++ src/Challenge.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
+import Dictionary from './Dictionary';
 
 class Challenge extends Component {
+  constructor() {
+    super();
+    this.state = {
+      challenges: [],
+      currentChallenge: 0
+    }
+  }
+
+  componentWillMount() {
+    this.setState( { challenges: [ this.createNewChallenge() ] } );
+  }
+
+  getNextChallenge = () => {
+    let challenges = this.state.challenges;
+    challenges.push( this.createNewChallenge() );
+
+    this.setState( {
+      challenges: challenges,
+      currentChallenge: this.state.currentChallenge + 1
+    } );
+  }
+
+  createNewChallenge = () => {
+    let id = this.state.challenges ? this.state.challenges.length : 0,
+        adjective = this.getRandomWord(Dictionary.adjectives),
+        lens = this.getRandomWord(Dictionary.lenses),
+        photo = this.getRandomWord(Dictionary.photo),
+        target = this.getRandomWord(Dictionary.target)
+
+    return {
+      id: id,
+      value: `A ${adjective} ${lens} ${photo} of ${target}`
+    };
+  }
+
+  getRandomWord = list => {
+    let index = Math.floor(Math.random() * list.length);
+    return list[index];
+  }
+
   render() {
     return (
-			<div className="Challenge">
-				<p className="Challenge__paragraph">
-					<span className="Challenge__text">A </span>
-					<span className="Challenge__highlight">wide angle </span>
-					<span className="Challenge__text">photo of </span>
-					<span className="Challenge__highlight">children playing</span>
-				</p>
+			<div className="Challenge"
+        onClick={ this.getNextChallenge }>
+        <p className="Challenge__paragraph">
+          <span className="Challenge__text">{ this.state.challenges[this.state.currentChallenge].value }</span>
+        </p>
 			</div>
     );
   }
---
+++ src/Dictionary.js
@@ -0,0 +1,28 @@
+const Dictionary = {
+	adjectives: [
+		'dark',
+		'colorful',
+		'bright',
+		'monochrome'
+	],
+	lenses: [
+		'wide angle',
+		'telephoto'
+	],
+	photo: [
+		'photo',
+		'photograph',
+		'photography',
+		'snapshot',
+		'picture'
+	],
+	target: [
+		'people',
+		'pets',
+		'a couple',
+		'a boy',
+		'a girl'
+	]
+}
+
+export default Dictionary;
